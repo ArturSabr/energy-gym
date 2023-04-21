2c32a328d12a93d88a292637617bf35287d9c530
Artur <gloory.gom@gmail.com>
2023-04-22 00:34:37 +0600

    configure all templates to base.html

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns= [
     path('', Home.as_view(), name='home'),
+    path('service/', Service.as_view(), name='service'),
+    path('about/', About.as_view(), name='about'),
+    path('contact/', Contact.as_view(), name='contact'),
 ]
--- app/views.py
+++ app/views.py
@@ -14,3 +14,41 @@ class Home(ListView):
         context['news'] = NewsModel.objects.all()
         return context
 
+
+class Service(ListView):
+    model = NewsModel
+    queryset = NewsModel.objects.all()[:3]
+    context_object_name = 'news'
+    template_name = 'service.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news'] = NewsModel.objects.all()
+        return context
+
+
+
+class About(ListView):
+    model = NewsModel
+    queryset = NewsModel.objects.all()[:3]
+    context_object_name = 'news'
+    template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news'] = NewsModel.objects.all()
+        return context
+
+
+
+class Contact(ListView):
+    model = NewsModel
+    queryset = NewsModel.objects.all()[:3]
+    context_object_name = 'news'
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news'] = NewsModel.objects.all()
+        return context
+
